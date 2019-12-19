fbc1d8e3cc735720420e236f7943ff4b4c3c5812
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-18 22:49:39 -0800

    solved user report bug #2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,38 +3,45 @@ class EventsController < ApplicationController
   before_action :find_event, except: [:index, :show, :feed, :create, :new]
   before_action :set_time_zone, only: [:create, :update]
   before_action :set_empty_location, only: [:new, :create]
-
+  
   def index
     skip_authorization
     respond_to do |format|
+      
       format.html do
         @events = Event.upcoming.published_or_visible_to(current_user)
-                    .includes(:location, :region, :chapter, :organization, event_sessions: :location)
+        .includes(:location, :region, :chapter, :organization, event_sessions: :location)
         @event_regions = @events.map(&:region).compact.uniq
+        
+        # line above is the same as these 2 lines below
+        # @event_regions = @events.map { |event| event.region }
+        # @event_regions = @event_regions.compact.uniq
       end
+      
       format.json do
         render json: EventList.new(params[:type], params.slice(:organization_id, :serialization_format, :start, :length, :draw, :search))
       end
+      
       format.csv do
         send_data EventList.new(EventList::ALL).to_csv, type: :csv
       end
     end
   end
-
+  
   def feed
     skip_authorization
     @events = Event.upcoming.published_or_visible_to(current_user).includes(:event_sessions, :location, :region)
-
+    
     respond_to do |format|
       format.rss {render 'events/feed.rss.builder', layout: false}
       format.atom {render 'events/feed.atom.builder', layout: false}
     end
   end
-
+  
   def levels
     skip_authorization
   end
-
+  
   def show
     skip_authorization
     @event = Event.includes(event_sessions: :location).find(params[:id])
@@ -62,22 +69,23 @@ class EventsController < ApplicationController
       end
     end
   end
-
+  
   def new
     skip_authorization
     @event = Event.new(public_email: current_user.email, time_zone: current_user.time_zone)
     @event.event_sessions << EventSession.new
   end
-
+  
   def edit
     authorize @event
   end
-
+  
   def create
     skip_authorization
     result = EventEditor.new(current_user, params).create
     @event = result.event
-
+    @events= Event.all    ### IDK... 
+    
     flash[:notice] = result.notice if result.notice
     if result.render
       render result.render
@@ -85,11 +93,11 @@ class EventsController < ApplicationController
       redirect_to result.event
     end
   end
-
+  
   def update
     authorize @event
     result = EventEditor.new(current_user, params).update(@event)
-
+    
     flash[:notice] = result.notice if result.notice
     if result.render
       render result.render, status: result.status
@@ -97,29 +105,29 @@ class EventsController < ApplicationController
       redirect_to @event
     end
   end
-
+  
   def destroy
     authorize @event
     @event.destroy
     redirect_to events_url
   end
-
+  
   protected
-
+  
   def set_time_zone
     if params[:event] && params[:event][:time_zone].present?
       Time.zone = params[:event][:time_zone]
     end
   end
-
+  
   def set_empty_location
     @location = Location.new
   end
-
+  
   def find_event
     @event = Event.find(params[:id])
   end
-
+  
   def allow_insecure?
     request.get? && (request.format.json? || request.format.csv?)
   end
